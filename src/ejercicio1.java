ae911216d1ec9424a68893f25186561acdaa968d
Ejercicio 1 <andersonreyez2006@gmail.com>
2026-07-04 18:53:09 -0600

    este es el primer ejercicio

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -1,41 +1,91 @@
 import javax.swing.*;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class ejercicio1 {
 
-public static double voltaje= 0;
-public static double carga;
-public static double capacitador;
-
-    public static void main (String[] args){
-        String voltajeText;
-        String cargaText;
-        String i = "==================================";
-        cargaText = JOptionPane.showInputDialog("digite carga (Q)");
-        voltajeText = JOptionPane.showInputDialog("ingrese voltaje (V)");
-
-        voltaje = Double.parseDouble(voltajeText);
-        carga = Double.parseDouble(cargaText);
-
-        System.out.println("\tDATOS INGRESADOS");
-        System.out.println(i);
-        System.out.println("la carga es de: " + carga);
-        System.out.println("El voltaje es de: "+ voltaje);
-
-
-        if (carga == 0) {
-            System.err.println("VALOR INVALIDO!!\nNO SE PUEDE DIVIDIR ENTRE 0!!");
-        } else if (voltaje < 0 || carga < 0) {
-            System.err.println("NO SE PUEDEN INGRESAR NUMERO NEGATIVOS");
+public static Double calc(double c1, double c2, double c3){
+    return c1+c2+c3;
+}
+public static double calc_s(double c1, double c2, double c3){
+    return (c1 * c2 * c3) / ((c2 * c3) + (c1 * c3) + (c1 * c2));
+}
+    public static void main(String[]args){
+        String c1t,c2t,c3t;
+        double c1 =0, c2=0, c3=0;
+        String sC1 = JOptionPane.showInputDialog("Capacitor 1:");
+        if (sC1 == null || sC1.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No ingresó un dato. El programa finalizará.");
+            System.exit(0);
+        } else {
+            try {
+                c1 = Double.parseDouble(sC1); // Convertimos
+                if (c1 <= 0) {
+                    JOptionPane.showMessageDialog(null, "Error: Los valores deben ser mayores a cero. El programa finalizará.");
+                    System.exit(0);
+                }
+            } catch (NumberFormatException e) { // Si puso letras en vez de números
+                JOptionPane.showMessageDialog(null, "Error: Valor no válido. El programa finalizará.");
+                System.exit(0);
+            }
         }
-        else {
-            cal_cap();
-            System.out.println(i);
-            System.out.println("El calculo realizado con la formula C = Q/V da como resultado de capacitancia: "+ capacitador);
+
+        // ==========================================
+        // CAPACITOR 2
+        // ==========================================
+        String sC2 = JOptionPane.showInputDialog("Capacitor 2:");
+        if (sC2 == null || sC2.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No ingresó un dato. El programa finalizará.");
+            System.exit(0);
+        } else {
+            try {
+                c2 = Double.parseDouble(sC2);
+                if (c2 <= 0) {
+                    JOptionPane.showMessageDialog(null, "Error: Los valores deben ser mayores a cero. El programa finalizará.");
+                    System.exit(0);
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Error: Valor no válido. El programa finalizará.");
+                System.exit(0);
+            }
+        }
+
+        // ==========================================
+        // CAPACITOR 3
+        // ==========================================
+        String sC3 = JOptionPane.showInputDialog("Capacitor 3:");
+        if (sC3 == null || sC3.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No ingresó un dato. El programa finalizará.");
+            System.exit(0);
+        } else {
+            try {
+                c3 = Double.parseDouble(sC3);
+                if (c3 <= 0) {
+                    JOptionPane.showMessageDialog(null, "Error: Los valores deben ser mayores a cero. El programa finalizará.");
+                    System.exit(0);
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Error: Valor no válido. El programa finalizará.");
+                System.exit(0);
+            }
+        }
+
+        double mostrarDatos1 = calc(c1, c2, c3);
+        double mostrarDatos2 = calc_s(c1, c2, c3);
+
+        String t = "==================================================================";
+        System.out.println(t);
+        System.out.println("               CONFIRMACION DE DATOS             ");
+        System.out.println(t);
+        System.out.println("Capacitor 1: " + c1 + " uF");
+        System.out.println("Capacitor 2: " + c2 + " uF");
+        System.out.println("Capacitor 3: " + c3 + " uF");
+        System.out.println();
+        System.out.println("=================================================");
+        System.out.println("                 RESULTADOS FINALES              ");
+        System.out.println("=================================================");
+        System.out.println("Capacitancia Equivalente en PARALELO: " + mostrarDatos1 + " uF");
+        System.out.println("Capacitancia Equivalente en SERIE: " + mostrarDatos2 + " uF");
+        System.out.println("=================================================");
         }
     }
 
-        public static void cal_cap () {
-            capacitador = carga / voltaje;
-        }
-
-}
